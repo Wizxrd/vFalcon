d7755b10c9699e5204416a8d201f92ee5da40bdf
Wizxrd <85634781+Wizxrd@users.noreply.github.com>
2025-07-25 00:19:04 -0400

    remove

--- obj/Debug/net8.0-windows/vFalcon.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/vFalcon.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("vFalcon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("0.0.2.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.2+8c1b38806d1bf6a7e2057a6e9174880c8d03b6c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.2+7f2a1dda7d9deec7d175c98ed11fd455fe94a733")]
 [assembly: System.Reflection.AssemblyProductAttribute("vFalcon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("vFalcon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("0.0.2.0")]
